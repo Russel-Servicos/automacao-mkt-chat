f650a8ca9d42289bcbd2a27759b34ff528edf924
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-09-18 20:50:21 -0300

    feat: finaliza a segunda automacao, adiciona a a contagem e taxa de conversao na coluna

--- automacao_mkt_chat_2de2.py
+++ automacao_mkt_chat_2de2.py
@@ -9,15 +9,14 @@ Original file is located at
 
 import pandas as pd
 import datetime
+import numpy as np
 
+caminho_do_arquivo = "/content/planilha_chat_estagio_2de2_2024_09_18_23_36.csv"
 caminho_do_arquivo = "https://raw.githubusercontent.com/Russel-Servicos/automacao-mkt-chat/main/planilha_chat_estagio_1de2_exemplo.csv"
 
 df = pd.read_csv(caminho_do_arquivo)
 
 data = "11/09/2024"
-
-# deixar unico os canais, origens, meios e campanhas, fazer o calculo com todos
-# usar comprehensive list para preencher o resto das linhas que falta para adicionar na coluna
 
 ## Conta quantidade de qualificado, não qualificado e emprego
 qty_ids = len(df.index)
@@ -25,28 +24,30 @@ qty_ids = len(df.index)
 qty_qualified = df.query("Qualificação == 'Qualificado'")
 qty_qualified = len(qty_qualified.index)
 percentage_qualified = qty_qualified / qty_ids
-percentage_qualified = "{:.2f}".format(percentage_qualified)
-percentage_qualified = float(percentage_qualified)
+percentage_qualified = "{:.0%}".format(percentage_qualified)
 
-qty_not_qualified = df.query("Qualificação == 'Não Qualificado'")
-qty_not_qualified = len(qty_not_qualified.index)
-percentage_not_qualified = qty_not_qualified / qty_ids
-percentage_not_qualified = "{:.2f}".format(percentage_not_qualified)
-percentage_not_qualified = float(percentage_not_qualified)
+## Muda os tipos de dados das colunas
+columns_to_type_conversions = {
+    'Canal': 'str',
+    'Origem':'str',
+    'Meio': 'str',
+    'Campanha': 'str',
+  }
+df = df.astype(columns_to_type_conversions)
 
-qty_emprego = df.query("Qualificação == 'Emprego'")
-qty_emprego = len(qty_emprego.index)
-percentage_emprego = qty_emprego / qty_ids
-percentage_emprego = "{:.2f}".format(percentage_emprego)
-percentage_emprego = float(percentage_emprego)
+clear_column_nan_value = lambda column_name: np.where(df[column_name] == 'nan', '', df[column_name])
 
-#
-unique = pd.unique(df["Campanha"])
-unique
-# remover nan da list
+df['Canal'] = clear_column_nan_value('Canal')
+df['Origem'] = clear_column_nan_value('Origem')
+df['Meio'] = clear_column_nan_value('Meio')
+df['Campanha'] = clear_column_nan_value('Campanha')
 
-# pega total de linhas, cria lista de contagem e taxa de conversao com valores e nan, joga na coluna
-df.shape
+## Busca os valores únicos dos canais
+canais = pd.unique(df["Canal"])
+origens = pd.unique(df["Origem"])
+meios = pd.unique(df["Meio"])
+campanhas = pd.unique(df["Campanha"])
+qualificacoes = pd.unique(df["Qualificação"])
 
 ## Contagem e Taxa de Conversão das colunas: Canal, Origem, Meio e Campanha
 def generate_data_column_contagem(values, column):
@@ -55,19 +56,17 @@ def generate_data_column_contagem(values, column):
   for value in values:
     df_filtered = df.query(f"{column} == '{value}'")
     total_rows = len(df_filtered.index)
+
     df_qualified = df_filtered.query("Qualificação == 'Qualificado'")
-    qty_qualified = len(df_qualified.index)
-    calculation = f"{value}: {total_rows} - {qty_qualified}"
+    qty_qualified_category = len(df_qualified.index)
 
-    if total_rows == 0:
-      percentage = 0.0
-    else:
-      percentage = qty_qualified / total_rows
+    calculation = f"{value}: {total_rows} - {qty_qualified_category}"
 
-    if percentage == 0.0:
-      percentage = 0
-    else:
+    if total_rows > 0:
+      percentage = qty_qualified_category / total_rows
       percentage = "{:.0%}".format(percentage)
+    else:
+      percentage = 0
 
     result_list.append({
         "TaxaConversão": percentage,
@@ -75,25 +74,48 @@ def generate_data_column_contagem(values, column):
     })
   return result_list
 
-canais = ["Adwords", "Direct", "Search Engine", "Referrals", "WhatsApp"]
-origens = ["google"]
-meios = ["display", "pesquisa", "youtube"]
-campanhas = ['aeroporto', 'barerestaurante', 'condominio', 'construcao', 'construcaorj', 'estacionamento', 'geral', 'logistica', 'lp-construcao', 'opmaquinas', 'pacotelimpeza', 'pacotelimpezarj', 'saude', 'terceirizacao', 'varejo', 'varejorj', 'ti', 'ygeral', 'yremarketing']
+def generate_data_column_contagem_from_qualificacao(values, column):
+  result_list = []
 
-result_canais = generate_data_column_contagem(values=canais, column="Canal")
-result_origens = generate_data_column_contagem(values=origens, column="Origem")
-result_meios = generate_data_column_contagem(values=meios, column="Meio")
-result_campanhas = generate_data_column_contagem(values=campanhas, column="Campanha")
+  for value in values:
+    df_filtered = df.query(f"{column} == '{value}'")
+    total_rows = len(df_filtered.index)
 
-result_campanhas
+    calculation = f"{value}: {total_rows}"
+
+    if total_rows > 0:
+      percentage = total_rows / qty_ids
+      percentage = "{:.0%}".format(percentage)
+    else:
+      percentage = 0
+
+    result_list.append({
+        "TaxaConversão": percentage,
+        "Calculo": calculation
+    })
+  return result_list
+
+contagem_canais_dict = generate_data_column_contagem(values=canais, column="Canal")
+contagem_origens_dict = generate_data_column_contagem(values=origens, column="Origem")
+contagem_meios_dict = generate_data_column_contagem(values=meios, column="Meio")
+contagem_campanhas_dict = generate_data_column_contagem(values=campanhas, column="Campanha")
+
+contagem_qualificacao_dict = generate_data_column_contagem_from_qualificacao(values=qualificacao, column="Qualificação")
 
 ## Insere valores na coluna Contagem e Taxa de Conversão
-all_result_columns = result_canais + [ {} ] + result_origens + [ {} ] + result_meios + [ {} ] + result_campanhas
+all_contagem_rows_dict = contagem_qualificacao_dict + [ {} ] + contagem_canais_dict + [ {} ] + contagem_origens_dict + [ {} ] + contagem_meios_dict + [ {} ] + contagem_campanhas_dict
+
+total_rows = df.shape[0]
+total_rows_contagem = len(all_contagem_rows_dict)
+total_rows_that_must_be_blank = total_rows - total_rows_contagem
+
+for _ in range(total_rows_that_must_be_blank):
+  all_contagem_rows_dict.append({})
 
 list_taxa_conversao = []
 list_contagem = []
 
-for value in all_result_columns:
+for value in all_contagem_rows_dict:
   dict_is_empty = len(value.keys()) == 0
 
   if dict_is_empty:
@@ -103,14 +125,8 @@ for value in all_result_columns:
     list_taxa_conversao.append(value["TaxaConversão"])
     list_contagem.append(value["Calculo"])
 
-
-# df.iloc[:, "Taxa de Conversão"] = list_taxa_conversao
-df
-# df.iloc[:, "Contagem"] = list_contagem
-
-df2 = pd.DataFrame([[1, 2], [4, 5], [7, 8], [9, 10], [11, 23]])
-df2.iloc[1:5,]
-# df2
+df.loc[:, "Taxa de Conversão"] = list_taxa_conversao
+df.loc[:, "Contagem"] = list_contagem
 
 ## Adiciona o valor Chat SalesIQ na coluna Canal
 df_salesiq = df.query("Canal in ['Adwords', 'Direct', 'Referrals', 'Search Engine']")
@@ -120,11 +136,8 @@ df_not_salesiq = df.query("Canal not in ['Adwords', 'Direct', 'Referrals', 'Sear
 
 df_final = pd.concat([df_salesiq, df_not_salesiq])
 
-df_final
-
 ## Adiciona valores totais na primeira linha, embaixo da linha das colunas
-df_final.loc[-1] = [data, qty_ids, qty_qualified, percentage_qualified, "", "",
-              "", "", "", "", "", "", ""]
+df_final.loc[-1] = [data, qty_ids, qty_qualified, percentage_qualified, "", "", "", "", "", "", "", ""]
 df_final.index = df_final.index + 1
 df_final = df_final.sort_index()
 
